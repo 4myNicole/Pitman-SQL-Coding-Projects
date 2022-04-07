12e41404ac8fa245066fa693d58b87539c120a0a
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-04-07 15:48:00 +0100

    Add files via upload

---
+++ Assignemnts 1-6.sql
@@ -0,0 +1,38 @@
+/* Assignment #1 */
+
+SELECT * 
+FROM tbl_habitat;
+
+/* Assignment #2 */
+
+SELECT species_name
+FROM tbl_species
+WHERE species_order = 3;
+
+/* Assignment #3 */
+
+SELECT nutrition_type
+FROM tbl_nutrition
+WHERE nutrition_cost <= 600.00
+
+/* Assignment #4 */
+
+SELECT species_name
+FROM tbl_nutrition
+WHERE nutrition_id BETWEEN 2202 AND 2206
+
+/* Assignment #5 */
+
+SELECT sp.species_name AS 'Species Name', nu.nutrition_id AS 'Nutrition Type'
+FROM tbl_species AS sp
+INNER JOIN tbl_nutrition AS nu
+ON nu.nutrition_id = sp.species_nutrition
+
+
+/* Assignment #6 */
+
+SELECT per.specialist_fname, per.specialist_lname, per.specialist_contact
+FROM tbl_specialist AS per
+INNER JOIN tbl_species AS spe
+ON per.specialist_id = spe.species_id
+WHERE spe.species_name = 'penguin'
